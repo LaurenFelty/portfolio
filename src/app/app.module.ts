a87dffe97d2e60afdc398b3372fe8c8d1784c2d0
Lauren <laurenfelty01@gmail.com>
2024-09-17 15:48:15 -0500

    update header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // Required for form handling
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
-import { HeaderComponent } from './shared/header.component';
-import { FooterComponent } from './shared/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 import { AppRoutingModule } from './app-routing.module'; // Import the routing module
 
 @NgModule({

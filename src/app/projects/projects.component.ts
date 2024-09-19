e5dc9cfc7cd7fe9af0a468d772e939ff9df2c5ec
Lauren <laurenfelty01@gmail.com>
2024-09-19 09:54:12 -0500

    0.1 version of portfolio

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,13 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent {
   projects = [
-    { title: 'WatchTower', description: 'A DynamoDB Capacity Visualization Tool', link: 'https://github.com/laurenfelty/watchtower' },
-    { title: 'BookBanter', description: 'A Book Club Management App', link: 'https://github.com/laurenfelty/bookbanter' },
-    { title: 'Bulls and Cows Game', description: 'A JavaScript-based algorithmic game', link: 'https://github.com/laurenfelty/bulls-and-cows' }
+    {
+      title: 'WatchTower',
+      description: 'A DynamoDB Capacity Visualization Tool',
+      link: 'https://github.com/oslabs-beta/watchtower',
+    },
+    {
+      title: 'Wander Wise',
+      description: 'A Travel Planning Application',
+      link: '#',
+    },
+    {
+      title: 'Pantry Partner',
+      description: 'A Pantry Inventory Application',
+      link: 'https://github.com/LaurenFelty/pantryPartner',
+    },
   ];
 }
-
